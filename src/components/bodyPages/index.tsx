b9f9dd58be8a7f6bd4b57fd51fefc04262e53da0
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-06-08 22:37:45 -0300

    Added component body

---
+++ src/components/bodyPages/index.tsx
@@ -0,0 +1,11 @@
+import './body.css'
+
+export default function body() {
+    return (
+        <div id="conteudo">
+            <div id="primario">Primario</div>
+            <div id="secundario">Secundario</div>
+            <div id="lateral">Lateral</div>
+        </div>
+    )
+}
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,10 +1,13 @@
 import Header from "../header"
+import Body from "../bodyPages"
 import '../../index.css'
 
 export default function home() {
     return (
         <div className="home">
             <Header />
+            <Body/>
+
         </div>
     )
 }
