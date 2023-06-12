453ad393ab7bc98b895f7217ebafbfeca4ba674d
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-06-12 07:58:28 -0300

    Add pagina notícias

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,13 +1,15 @@
 import Header from "../header"
 import Body from "../body-pages"
 import BaseBoard from "../baseboard"
+import PageNoticia from "../pages/page-noticias.tsx"
 import '../../index.css'
 
 export default function home() {
     return (
         <div className="home">
             <Header />
-            <Body />
+            {/* <Body /> */}
+            <PageNoticia/>
             <BaseBoard />
         </div>
     )
