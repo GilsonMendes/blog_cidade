2a1c4d33dce9d04bb7515eb172c0f714a22bd1a0
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-06-15 21:33:53 -0300

    Add conteiners da pagina galeria de fotos

--- src/components/pages/galeria-fotos.tsx
+++ src/components/pages/galeria-fotos.tsx
@@ -1,7 +1,16 @@
-export default function galeria(){
-    return(
-        <div>
-            Galeria
+import './galeria.css'
+
+export default function galeria() {
+    return (
+        <div className="container">
+            <div id='conteiner-fotos'>
+                <div className="galeria-fotos">
+
+                </div>
+                <div className="galeria-fotos">
+
+                </div>
+            </div>
         </div>
     )
 }
