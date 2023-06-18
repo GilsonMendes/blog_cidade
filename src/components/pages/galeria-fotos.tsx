6a6121075481350caaf303dfd285f739c580d5b6
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-06-17 22:13:19 -0300

    Add correções da galeria

--- src/components/pages/galeria-fotos.tsx
+++ src/components/pages/galeria-fotos.tsx
@@ -3,13 +3,36 @@ import './galeria.css'
 export default function galeria() {
     return (
         <div className="container">
-            <div id='conteiner-fotos'>
-                <div className="galeria-fotos">
+            <h1>Galeria de Fotos</h1>
+            <div id='container-fotos'>
+
+                <div className="galeria-fotos-a">
+                    <img src="../../../public/Campeonato.jpg" alt="campeonato" />
 
                 </div>
-                <div className="galeria-fotos">
+                <div className="galeria-fotos-a">
+                    <img src="../../../public/Novo jogo.jpg" alt="campeonato" />
 
                 </div>
+
+                <div className="galeria-fotos-a">
+                    <img src="../../../public/atleta.jpg" alt="campeonato" />
+
+
+                </div>
+                <div className="galeria-fotos-a">
+                    <img src="../../../public/mulher-jovem.jpg" alt="campeonato" />
+
+                </div>
+                <div className="galeria-fotos-b">
+                    <img src="../../../public/mundo.jpg" alt="campeonato" />
+
+                </div>
+                <div className="galeria-fotos-b">
+                    <img src="../../../public/cidade.jpg" alt="campeonato" />
+
+                </div>
+
             </div>
         </div>
     )
